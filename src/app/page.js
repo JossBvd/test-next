0404044ae0b2b4b74dbbed0d61ed7bdd344e2b6b
Jocelyn Bonvard <113903989+JossBvd@users.noreply.github.com>
2025-04-01 16:16:54 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,11 @@ export default function Home() {
     <div className="">
       <h1>Collaborateur</h1>
       <ol>
-        <li>Stéphane "créateur de la page"</li>
+        
         <li>Jocelyn</li>
 
         <li>Alex</li>
+        <li class="text-sm">Stéphane</li>
       </ol>
     </div>
   );
